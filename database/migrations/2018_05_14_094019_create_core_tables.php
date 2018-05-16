f263c0ab080a509de5f179067ec8f47fdc48b6ff
Anthony Vanden Bossche <toonevdb@gmail.com>
2018-05-16 11:50:08 +0200

    task: core tables migration

--- database/migrations/2018_05_14_094019_create_core_tables.php
+++ database/migrations/2018_05_14_094019_create_core_tables.php
@@ -13,7 +13,64 @@ class CreateCoreTables extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('content', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('type');
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')
+              ->onUpdate('cascade')->onDelete('restrict');
+        });
+
+        Schema::create('derived_content', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('content_id')->unsigned();
+            $table->string('name');
+            $table->string('plugin');
+
+            $table->unique(['name', 'plugin']);
+
+            $table->foreign('content_id')->references('id')->on('content')
+              ->onUpdate('cascade')->onDelete('cascade');
+        });
+
+        Schema::create('roles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->unique();
+            $table->timestamps();
+        });
+
+        Schema::create('user_roles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('role_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')
+              ->onUpdate('cascade')->onDelete('cascade');
+            
+            $table->foreign('role_id')->references('id')->on('roles')
+              ->onUpdate('cascade')->onDelete('restrict');
+        });
+
+        Schema::create('permissions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('action');
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->integer('role_id')->unsigned()->nullable();
+            $table->integer('content_id')->unsigned()->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('plugins', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +80,11 @@ class CreateCoreTables extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('plugins');
+        Schema::dropIfExists('permissions');
+        Schema::dropIfExists('user_roles');
+        Schema::dropIfExists('roles');
+        Schema::dropIfExists('derived_content');
+        Schema::dropIfExists('content');
     }
 }
